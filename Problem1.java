c223fb0fb9e4aff642dbd2eecf2a9fd6065370cc
Prakruthi Shivram <prakruthis18@gmail.com>
2024-06-24 17:08:27 -0700

    competetive programming 1 done

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,27 @@
 
+// "static void main" must be defined in a public class.
+public class Problem1 {
+    public static void main(String[] args) {
+        int[] nums = new int []{1,2,3,4,6,7,8,9};
+        int result = getMissing(nums);
+       System.out.println(result);
+    }
+    
+    public static int getMissing(int[] arr){
+        int low = 0;
+        int high = arr.length -1;
+        
+        while(low<=high){
+            int mid = low+ (high-low)/2;
+            if(arr[mid]!=mid+1 && ( arr[mid-1] == mid)){
+                return mid+1;
+            }
+            if(arr[low] == 1+low  && arr[mid] == mid +1 ){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,126 @@
+public class Problem2
+{
 
+	private int[] Heap;
+	private int size;
+	private int capacity;
+	private static final int FRONT = 1;
+
+	public Problem2(int capacity)
+	{
+		this.capacity = capacity;
+		this.size = 0;
+
+		Heap = new int[this.capacity + 1];
+		Heap[0] = Integer.MIN_VALUE;
+	}
+
+	private int parent(int pos) { 
+        return pos / 2; 
+    }
+
+	private int leftChild(int pos) { 
+        return (2 * pos); 
+    }
+
+	private int rightChild(int pos)
+	{
+		return (2 * pos) + 1;
+	}
+
+	private boolean isLeaf(int pos)
+	{
+
+		return pos > (size / 2) && pos <= size;
+	}
+
+	private void swap(int i, int j)
+	{
+		int temp;
+		temp = Heap[i];
+		Heap[i] = Heap[j];
+		Heap[j] = temp;
+	}
+
+	
+private void minHeapify(int pos)
+{	 
+	if(!isLeaf(pos)){
+	int swapPos= pos;
+    
+	if(rightChild(pos)<=size)
+		swapPos = Heap[leftChild(pos)]<Heap[rightChild(pos)]?leftChild(pos):rightChild(pos);
+	else
+		swapPos= leftChild(pos);
+		
+	if(Heap[pos]>Heap[leftChild(pos)] || Heap[pos]> Heap[rightChild(pos)]){
+		swap(pos,swapPos);
+		minHeapify(swapPos);
+	}
+		
+	}	 
+}
+
+	public void insert(int element)
+	{
+
+		if (size >= capacity) {
+			return;
+		}
+
+		Heap[++size] = element;
+		int current = size;
+
+		while (Heap[current] < Heap[parent(current)]) {
+			swap(current, parent(current));
+			current = parent(current);
+		}
+	}
+
+	public void print()
+	{
+		for (int i = 1; i <= size / 2; i++) {
+
+			System.out.print(
+				" PARENT : " + Heap[i]
+				+ " LEFT CHILD : " + Heap[2 * i]
+				+ " RIGHT CHILD :" + Heap[2 * i + 1]);
+
+			System.out.println();
+		}
+	}
+
+	
+	public int remove()
+	{
+
+		int popped = Heap[FRONT];
+		Heap[FRONT] = Heap[size--];
+		minHeapify(FRONT);
+
+		return popped;
+	}
+
+    public static void main(String[] arg)
+	{
+
+		System.out.println("The Min Heap is ");
+
+		Problem2 minHeap = new Problem2(15);
+
+		minHeap.insert(5);
+		minHeap.insert(3);
+		minHeap.insert(17);
+		minHeap.insert(10);
+		minHeap.insert(84);
+		minHeap.insert(19);
+		minHeap.insert(6);
+		minHeap.insert(22);
+		minHeap.insert(9);
+
+		minHeap.print();
+
+		System.out.println("The Min val is "
+						+ minHeap.remove());
+	}
+}
